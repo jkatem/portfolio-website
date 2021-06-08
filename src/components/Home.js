dfd8d660ecd549425d944afe99d10fd042b987e8
jkatem <jenmiao@gmail.com>
2021-06-07 23:39:45 -0700

    add about section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import About from './About'
+import './Home.css'
+import { Link } from 'react-router-dom'
+
 
 const Home = () => {
     return (
-        <div>
-            
+        <div className="home-page">
+            <h1 className="abouth1">
+                <Link to='/'>JENNIFER MIAO</Link> | Software Engineer 
+            </h1>
+            <About />
         </div>
     )
 }
