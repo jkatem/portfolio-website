7e001e1156f42237314551958e02bde1bd0b85f0
jkatem <jenmiao@gmail.com>
2021-06-10 21:58:57 -0700

    add link to Linkedin and more css

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,23 @@
 import React from 'react'
+import Me from '../Images/Me.jpeg'
 import { Link } from 'react-router-dom'
 import './About.css'
 
 function About() {
     return (
-        <div id="About" className="aboutsection">            
-            <p className="aboutbodytext">
-                I am a Software Engineer based out of San Francisco, CA with 
+        <div id="About" >
+            <div className="flex-container">
+                <img src={Me} alt="Profile Pic"/>
+            
+                I am a Software Engineer based out of San Francisco, CA with a background in the legal field. akfjsakdfjkasfjsdfkaskfjsjdf akslfksajdf akslfj klsadfjaskf. Jkdfjs. ksdfjskd asjfksf aksfjkjf asfjskj.
                 {/* Skills I want to mention: research, strong communication    */}
-            <br/>
-            <br/>
-            Interested in learning more?
+            </div>
+            <p>
+            Interested in learning more? 
+            
+            <Link to="/PDFResume">Resume</Link> | <Link to="/linkedin">Linkedin</Link> | <Link to="/Github">Github</Link>
             </p>
-            <Link to="/PDFResume">Resume</Link> | <Link to="/linkedin">Linkedin</Link> | <Link to="/github">Github</Link>
+            
         </div>
     )
 }
