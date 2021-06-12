078db81eaca661fbda2b5dd6fda273c9feb3a29a
jkatem <jenmiao@gmail.com>
2021-06-12 12:00:58 -0700

    add links to git projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './Project.css'
+import { Link } from 'react-router-dom'
 
 const Project = () => {
     return (
         <div className="projects">
             <h3>DocuManage</h3>
-
+            <Link to='https://github.com/jkatem/DocuManage'>sss</Link>
+            
+            <h3>Sniff-n-play</h3>
+            <Link to="https://github.com/jkatem/sniff_and_play">fksjfsjldfjjsf</Link>
         </div>
     )
 
