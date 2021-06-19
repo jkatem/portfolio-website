68c2659364b33569011b3c923374bcff48726037
jkatem <jenmiao@gmail.com>
2021-06-19 11:58:51 -0700

    start adding carousel feature to Project Section

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import './Project.css'
-import { Link } from 'react-router-dom'
+import test1 from '../Images/test1.jpeg'
+import test2 from '../Images/test2.jpeg'
+import test3 from '../Images/test3.jpeg'
+
+// import { Link } from 'react-router-dom'
+
+// const Project = () => {
+//     return (
+//         <div className="projects">
+//             <h3>DocuManage</h3>
+//             <Link to='https://github.com/jkatem/DocuManage'>sss</Link>
+            
+//             <h3>Sniff-n-play</h3>
+//             <Link to="https://github.com/jkatem/sniff_and_play">fksjfsjldfjjsf</Link>
+//         </div>
+//     )
+
+// }
+// export default Project;
 
 const Project = () => {
     return (
-        <div className="projects">
-            <h3>DocuManage</h3>
-            <Link to='https://github.com/jkatem/DocuManage'>sss</Link>
-            
-            <h3>Sniff-n-play</h3>
-            <Link to="https://github.com/jkatem/sniff_and_play">fksjfsjldfjjsf</Link>
-        </div>
-    )
+        <div>
+            <a className="arrow prev">Prev</a>
+            <a className="arrow next">Next</a>  
 
+            <ul id="carousel">
+                <li class="visible"><a href="https://github.com/jkatem/DocuManage"><img src={test1} alt="" /></a></li>
+                <li class="visible"><a href="https://github.com/jkatem/sniff_and_play"><img src={test2} alt="" /></a></li>
+                <li class="visible"><a href="https://github.com/jkatem/sniff_and_play"><img src={test3} alt="" /></a></li>
+
+            </ul>
+        </div>
+        
+    )
 }
-export default Project;
+export default Project
