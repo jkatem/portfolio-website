931bd10d36185c97573fd3c26ea35e7009996af3
jkatem <jenmiao@gmail.com>
2021-07-08 11:33:46 -0700

    rebuilt navbar and items align correctly

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,80 +1,76 @@
-import React from 'react';
-import { Box, Flex, Link as ChakraLink, chakra, useColorModeValue } from '@chakra-ui/react'
-// import { Link } from 'react-router-dom' 
-// import Link from 'next/link'
+import React from 'react'
+// import { Link } from "react-router-dom"
+import { Navbar, Nav } from 'react-bootstrap'
 
+// const HeaderItems = props => {
+//   const { children, to="/"} = props
+//   return (
+//     <Text
+//       display="block"
+//     >
+//     <Link to={to}>{children}</Link>
+//     </Text>
+//   )
+// }
 
+const Header = props => {
+  return (
+    // <>
+    // <div class='container' >
+    //   <div class='row'>
+    //     <div class='col' >
+    //     <Navbar expand="lg" class="navbar-brand">
+    //       JENNIFER MIAO
+    //     </Navbar>
+    //     </div>
+    //     <ul class="nav justify-content-end">
+    //       <li>
+    //         <div class='col'>
+    //           <Nav.Item>
+    //             <Nav.Link>About</Nav.Link>
+    //           </Nav.Item>
+    //         </div>
+    //       </li>
+    //       <li>
+    //         <div class='col'>
+    //         <Nav.Item>
+    //           <Nav.Link>Projects</Nav.Link>
+    //         </Nav.Item>
+    //         </div>
+    //       </li>   
+    //     </ul>
+    //   </div>
+    // </div>
+    // </>
+    <>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+      <text>JENNIFER MIAO</text>      
+        <ul class="nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          
+          <li class="nav-item">
+            <a class="nav-link" href="#">Projects</a>
+          </li>
+          
+          {/* <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+            <div class="dropdown-menu" aria-labelledby="dropdown01">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li> */}
+        </ul>
+    </nav>
 
-const Links = [
-    {
-        name: 'Projects',
-        link: '/projects'
-    },
-    {
-        name: 'About',
-        link: '/about'
-    }
-]
-
-
-const Navbar = () => {
-    return (
-        <chakra.header
-            width='full'
-            position='fixed'
-            top={0}
-            left={0}
-            zIndex={10}
-            bg={useColorModeValue(`rgba(255, 255, 255, 0.8)`, `rgba(26, 33, 41, 0.8)`)}
-            sx={{ backdropFilter: `saturate(180%) blur(5px)` }}
-        >
-            
-        <chakra.nav mx='auto' p={3}>
-            <Flex
-            margin='auto'
-            justifyContent='space-between'
-            alignContent='center'
-            maxW='7xl'
-            width='full'
-            >
-            <Box display='flex' alignContent='center'>
-            <Links> 
-                <ChakraLink
-                    fontSize='lg'
-                    margin='auto'
-                    fontWeight='semibold'
-                    position='relative'
-                    textTransform='capitalize'
-                    _after={{
-                    transition: `all 0.25s ease-in-out`,
-                    content: `''`,
-                    /* Fixes anti-aliasing issue in chrome that leaves one pixel' */
-                    outline: `1px solid transparent`,
-                    width: `0%`,
-                    height: `30%`,
-                    position: `absolute`,
-                    bottom: 1,
-                    left: 0,
-                    bg: useColorModeValue(`brand.200`, `brand.700`),
-                    zIndex: -1,
-                    }}
-                    _hover={{
-                    _after: {
-                        width: `100%`,
-                    },
-                    color: useColorModeValue(`gray.900`, `white`),
-                    }}
-                >
-                JENNIFER MIAO | Software Engineer
-              </ChakraLink>
-            </Links>
-          </Box>
-          {/* <MobileNav links={links} />
-          <DesktopNav links={links} /> */}
-        </Flex>
-      </chakra.nav>
-    </chakra.header>
-    )
+        </>
+  )
 }
 
-export default Navbar
+export default Header
+
