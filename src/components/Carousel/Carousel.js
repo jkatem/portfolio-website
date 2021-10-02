65a25bb73400f625279e727085c1c9bcdba96ceb
jkatem <jenmiao@gmail.com>
2021-10-02 13:32:12 -0700

    start build Carousel Component

---
+++ src/components/Carousel/Carousel.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Carousel extends React.Component {
+
+    render(){
+        return(
+            <div>
+                <h3>My Carousel Component</h3>
+            </div>
+        )
+    }
+}
+
+export default Carousel
