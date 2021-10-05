3225edcf54a717d8d44f6210609ba1b6517eeb8b
jkatem <jenmiao@gmail.com>
2021-10-05 15:07:02 -0700

    create SliderData component to include my projects

---
+++ src/components/Carousel/SliderData.js
@@ -0,0 +1,11 @@
+// import React from 'react'
+import test1 from './Images/test1.jpeg'
+import test2 from './Images/test2.jpeg'
+import test3 from './Images/test3.jpeg'
+
+
+export const SliderData = [
+    { project: test1 },
+    { project: test2 },
+    { project: test3 } 
+]
