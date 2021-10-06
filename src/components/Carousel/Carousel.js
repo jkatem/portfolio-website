e71bdef9f3c019a47625b2be21fab3cacf6356c4
jkatem <jenmiao@gmail.com>
2021-10-05 19:38:57 -0700

    remove Arrow component

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,14 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { SliderData } from './SliderData'
+import {FaArrowAltCircleRight, FaArrowAltCircleLeft} from 'react-icons/fa'  
 
-class Carousel extends React.Component {
+const Carousel = ({projects}) => {
 
-    render(){
-        return(
-            <div>
-                <h3>My Carousel Component</h3>
-            </div>
-        )
+    console.log(projects)
+
+    const [current, setCurrent] = useState(0);
+    const length = projects.length 
+
+    const nextSlide = () => {
+        setCurrent(current === length - 1 ? 0 : current + 1)
     }
+
+    if (!Array.isArray(projects) || projects.length <= 0) {
+        return null 
+    }
+
+    console.log(nextSlide)
+
+    return (
+        <section className="slider">
+            <FaArrowAltCircleLeft className='left-arrow' />           
+        
+            {SliderData.map((slide, index) => {
+                return (
+                    // console.log(slide)
+                    <img src={slide.project} alt="test" />
+                )
+            })}
+            <FaArrowAltCircleRight className='right-arrow' onClick={nextSlide} />
+        </section>
+    )
 }
 
-export default Carousel
+export default Carousel; 
