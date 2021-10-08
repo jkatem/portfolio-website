53148f37bdfc9c0c966a6d712dcbffc80cdda8ca
jkatem <jenmiao@gmail.com>
2021-10-07 21:03:55 -0700

    build logic for right/left arrow

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -4,7 +4,7 @@ import {FaArrowAltCircleRight, FaArrowAltCircleLeft} from 'react-icons/fa'
 
 const Carousel = ({projects}) => {
 
-    console.log(projects)
+    // console.log(projects)
 
     const [current, setCurrent] = useState(0);
     const length = projects.length 
@@ -13,20 +13,27 @@ const Carousel = ({projects}) => {
         setCurrent(current === length - 1 ? 0 : current + 1)
     }
 
+    const prevSlide = () => {
+        // console.log(`I am prevSlide`)
+        setCurrent(current === 0 ? length - 1 : current - 1)
+    }
+
+    console.log(current)
+
     if (!Array.isArray(projects) || projects.length <= 0) {
         return null 
     }
 
-    console.log(nextSlide)
-
     return (
         <section className="slider">
-            <FaArrowAltCircleLeft className='left-arrow' />           
+            <FaArrowAltCircleLeft className='left-arrow' onClick={prevSlide} />           
         
             {SliderData.map((slide, index) => {
                 return (
-                    // console.log(slide)
-                    <img src={slide.project} alt="test" />
+                    <div key={index}>
+                        {index === current && (<img src={slide.project} alt="test" />)}
+                     
+                    </div>                   
                 )
             })}
             <FaArrowAltCircleRight className='right-arrow' onClick={nextSlide} />
