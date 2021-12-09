1b468a81d0a600ce7ef2c040252e4e6a544b68d9
jkatem <jenmiao@gmail.com>
2021-12-08 22:53:19 -0800

    est foot below my projects section

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,16 +1,42 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
-
+import React from "react";
+import {
+  Box,
+  Container,
+  Row,
+  Column,
+  FooterLink,
+  Heading,
+} from "./FooterStyle";
+  
 const Footer = () => {
-    return (
-        <>
-        <div>
-           <h1>Footer</h1>
-            <Link to="https://github.com/jkatem">Github</Link>
-        </div>
-        </>
-
-    )
-}
-
-export default Footer
+  return (
+    <Box>
+      <Container>  
+          <Row>
+            <FooterLink href="#">
+              <i className="fab fa-instagram">
+                <span style={{ marginLeft: "10px" }}>
+                  Instagram
+                </span>
+              </i>
+            </FooterLink>
+            <FooterLink href="#">
+              <i className="fab fa-twitter">
+                <span style={{ marginLeft: "10px" }}>
+                  Twitter
+                </span>
+              </i>
+            </FooterLink>
+            <FooterLink href="#">
+              <i className="fab fa-youtube">
+                <span style={{ marginLeft: "10px" }}>
+                  Youtube
+                </span>
+              </i>
+            </FooterLink>
+          </Row>
+      </Container>
+    </Box>
+  );
+};
+export default Footer;
