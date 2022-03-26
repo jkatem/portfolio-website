9ab91b4f1f629facb4a8a476766e2d2fdf1d0c61
jkatem <jenmiao@gmail.com>
2022-03-25 21:36:33 -0700

    add link to social media capabilities to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,17 @@
 import React from "react";
 import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
-import { makeStyles, Theme } from '@material-ui/core/styles';
-import StyledLink from '../StyledLink';
+import { NavLink, Link } from 'react-router-dom'
 
-const useStyles = makeStyles((theme: Theme) => {
-  return {
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+// import LinkedInIcon from '@mui/icons-material/LinkedIn';
+// import BottomNavigation from '@mui/material/BottomNavigation';
+// import StyledLink from '../StyledLink';
+
+const useStyles = makeStyles(theme => ({
     main: {
       backgroundColor: '#C4C4C4',
-      padding: theme.spacing(5),
+      padding: theme.spacing(3),
     },
     linkBlock: {
       '& > *': {
@@ -21,8 +24,7 @@ const useStyles = makeStyles((theme: Theme) => {
     bottom: {
       marginTop: theme.spacing(4),
     },
-  };
-});
+}));
 
   
 function Footer() {
@@ -30,7 +32,8 @@ function Footer() {
   const classes = useStyles();
 
   return (
-    <Grid className="Footer">
+    <>
+        <Grid className="Footer">
       <Grid className={classes.main} container>  
           <Grid
             className={classes.linkBock}
@@ -40,14 +43,20 @@ function Footer() {
             direction="column"
             alignItems="flex-start"
           >
-            <Typography 
-              className={classes.header} align="center" 
-              gutterBottom>Jennifer Miao's Website
-            </Typography>
-            <StyledLink to="/About">About</StyledLink>
-          </Grid>
+          <span>
+            <a href="https://github.com/jkatem">Github</a>&nbsp;&nbsp;
+            <a href="https://linkedin.com/in/jenmiao">Linkedin</a>&nbsp;&nbsp;
+          </span>  
+          </Grid>        
+        </Grid>
+    </Grid>
+    </>
 
-            {/* <FooterLink href="#">
+  );
+};
+
+
+           /* <FooterLink href="#">
               <i className="fab fa-instagram">
                 <span style={{ marginLeft: "10px" }}>
                   Instagram
@@ -67,9 +76,11 @@ function Footer() {
                   Youtube
                 </span>
               </i>
-            </FooterLink> */}
-        </Grid>
-    </Grid>
-  );
-};
+            </FooterLink> */
+
 export default Footer;
+
+{/* <Typography             
+              className={classes.header} align="center" 
+              gutterBottom>Jennifer Miao's Website 
+            </Typography>  */}
